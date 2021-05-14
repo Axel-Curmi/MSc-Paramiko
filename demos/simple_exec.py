b11040c61e48630746aa52626bb2b9a432a675f4
Axel Curmi <axel.curmi@gmail.com>
2021-05-14 13:04:16 +0200

    update simple_exec test driver

--- demos/simple_exec.py
+++ demos/simple_exec.py
@@ -1,60 +1,57 @@
 from paramiko import (SSHClient)
 from paramiko.config import SSH_PORT
 
-import logging
-
 from pysecube.wrapper import Wrapper
 
-logging.basicConfig()
-logging.getLogger("paramiko").setLevel(logging.DEBUG)
+import time
 
+# import logging
 # logging.basicConfig()
-# logging.getLogger("pysecube").setLevel(logging.DEBUG)
+# logging.getLogger("paramiko").setLevel(logging.DEBUG)
 
 PYSECUBE_PIN = b"test"
+HOSTNAME = "192.168.37.136"
+USERNAME = "user"
+PASSWORD = "password"
+COMMAND = "uname"
 
 def main() -> int:
-    from argparse import ArgumentParser
-
-    parser = ArgumentParser(
-        description="Execute command on remote host with SSH")
-    parser.add_argument("--host", "-H", type=str, required=True)
-    parser.add_argument("--username", "-u", type=str, required=True)
-    parser.add_argument("--password", "-p", type=str, required=True)
-    parser.add_argument("--command", "-c", type=str, required=True)
-    args = parser.parse_args()
-
-    pysecube = Wrapper(b"test")
+    pysecube = Wrapper(PYSECUBE_PIN)
     pysecube.crypto_set_time_now()
 
-    try:
-        with SSHClient() as client:
-            client.load_system_host_keys()
+    client = SSHClient()
+    client.load_system_host_keys()
 
-            client.connect(args.host, SSH_PORT, args.username, args.password,
-                        disabled_algorithms={
-                            # Force KEX engine to use DH Group 14 with SHA256
-                            "kex": [
-                                    "curve25519-sha256@libssh.org",
-                                    "ecdh-sha2-nistp256",
-                                    "ecdh-sha2-nistp384",
-                                    "ecdh-sha2-nistp521",
-                                    "diffie-hellman-group16-sha512",
-                                    "diffie-hellman-group-exchange-sha256",
-                                    "diffie-hellman-group-exchange-sha1",
-                                    "diffie-hellman-group14-sha1",
-                                    "diffie-hellman-group1-sha1",
-                            ]
-                        },
-                        pysecube=pysecube
-            )
-            # client.connect(args.host, SSH_PORT, args.username, args.password)
-            print(f"Connected with {args.host}")
+    print(f"Connecting with {HOSTNAME}:{SSH_PORT}")
+    client.connect(HOSTNAME, SSH_PORT, USERNAME, PASSWORD, disabled_algorithms={
+        "kex": [
+            "curve25519-sha256@libssh.org",
+            "ecdh-sha2-nistp256",
+            "ecdh-sha2-nistp384",
+            "ecdh-sha2-nistp521",
+            "diffie-hellman-group16-sha512",
+            "diffie-hellman-group-exchange-sha256",
+            "diffie-hellman-group-exchange-sha1",
+            "diffie-hellman-group14-sha1",
+            "diffie-hellman-group1-sha1",
+        ]
+    }, pysecube=pysecube)
+    print("Connected successfully")
 
-            _, stdout, _ = client.exec_command(args.command)
-            print(stdout.read().decode())
-    except Exception:
-        return 1
+    channel = client.get_transport().open_channel("session")
+    channel.exec_command("uname -a")
+    stdout = channel.makefile("r", -1)
+
+    # Wait for an EOF to be received
+    while not channel.eof_received:
+        time.sleep(1)
+
+    channel.close()
+    print(stdout.read().decode())
+    stdout.close()
+
+    client.close()
+    pysecube.destroy()
     return 0
 
 if __name__ == "__main__":
